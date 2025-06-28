89bd656d84cba022047a0d7e9acec5cd2cd7f0f9
Swetlana Chitranshu <swetlanac3@gmail.com>
2025-06-28 11:49:01 +0530

    Initial commit - Node.js SSR Backend setup

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use('/uploads', express.static('uploads'));
+
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose.connect(MONGO_URI)
+    .then(() => console.log("MongoDB connected"))
+    .catch(err => console.log(err));
+
+app.get('/', (req, res) => res.send('API is running...'));
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
